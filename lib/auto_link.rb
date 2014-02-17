21495612537d5bda83d9358f6dac7cb8bb174bad
Salman Ansari <salman.ansari@gmail.com>
2014-02-17 14:29:52 -0500

    use activesupport concern for autolink module

--- lib/auto_link.rb
+++ lib/auto_link.rb
@@ -1,12 +1,10 @@
 module AutoLink
+  extend ActiveSupport::Concern
+
   REGEX_MAP = {:mentions => "@[0-9A-Za-z_]+", :hashtags => "#[0-9A-Za-z_]+"}
 
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
-
-  module ClassMethods
-    def auto_link(target, options={})
+  included do
+    def self.auto_link(target, options={})
       define_method "linked_#{options[:as] || target}" do
         matchers = Array(options[:on])
         regex = Array(options[:on]).map { |matcher| REGEX_MAP[matcher] }.join('|')
